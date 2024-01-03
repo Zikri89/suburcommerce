d87ead4dc134a88370f64642d207dc7fd07fa191
Zikri89 <asalngoding89@gmail.com>
2024-01-03 13:45:43 +0700

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,20 @@
-import { Component, ElementRef  } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ElementRef } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../../hero/hero.component';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroComponent],
+  imports: [CommonModule, HeroComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   constructor(private route: ActivatedRoute, private el: ElementRef) {}
   ngAfterViewInit() {
-    this.route.fragment.subscribe(fragment => {
+    this.route.fragment.subscribe((fragment) => {
       if (fragment) {
         const element = this.el.nativeElement.querySelector(`.${fragment}`);
         if (element) {
